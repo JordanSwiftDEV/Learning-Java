eaca0f6dc380707d8ea0d42592efd7e4c804817f
Jordan Swift <jordanswiftdev@gmail.com>
2017-06-03 11:09:26 +0100

    Add files via upload

---
+++ Getting User Input/src/Test.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Test {
+	public static void main(String args[]){
+		Scanner jordan = new Scanner(System.in);
+		System.out.println(jordan.nextLine());
+	}
+}
