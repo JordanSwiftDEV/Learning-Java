6e5f246eb22c981af760af961f8fa49d70eb0ee5
Jordan Swift <jordanswiftdev@gmail.com>
2017-06-03 19:21:30 +0100

    Add files via upload

---
+++ Summing Elements of Arrays/src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+	public static void main(String[] args){
+		int jordan[]={21,16,86,21,3};
+		int total=0;
+		
+		for(int counter=0;counter<jordan.length;counter++){
+		total+=jordan[counter];
+		}
+		System.out.println("The sum of these number is " + total);
+	}
+}
