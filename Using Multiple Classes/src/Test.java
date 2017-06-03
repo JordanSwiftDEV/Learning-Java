1743d83149ed0d4c16fcdac9569d85705a3675c6
Jordan Swift <jordanswiftdev@gmail.com>
2017-06-03 11:07:57 +0100

    Add files via upload

---
+++ Using Multiple Classes/src/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+	public static void main(String[] args){
+		
+		testing testingObject = new testing();
+		testingObject.simpleMessage();
+		
+	}
+
+}
---
+++ Using Multiple Classes/src/testing.java
@@ -0,0 +1,7 @@
+
+public class testing {
+	public void simpleMessage(){
+		System.out.println("This is in another class");
+	}
+
+}
