03b49a3c435100d1a9ad24895eaeb39d39081e4e
Jordan Swift <jordanswiftdev@gmail.com>
2017-06-03 11:05:21 +0100

    Add files via upload

---
+++ Increment Operators/src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+	public static void main(String args[]){
+		
+		int tuna = 5;
+		
+		tuna *= 8; //assignment operator
+		System.out.println(tuna++); //post incrementing
+		
+		System.out.println(tuna);
+	}
+}
