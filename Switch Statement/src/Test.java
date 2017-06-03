b67a43b4b2b6616da2128f3e655a53cc195c98e3
Jordan Swift <jordanswiftdev@gmail.com>
2017-06-03 11:07:17 +0100

    Add files via upload

---
+++ Switch Statement/src/Test.java
@@ -0,0 +1,21 @@
+public class Test {
+	public static void main(String args[]){
+		int age;
+		age = 2;
+		
+		switch (age){
+		case 1:
+			System.out.println("You can crawl");
+			break;
+		case 2:
+			System.out.println("You can talk");
+			break;
+		case 3:
+			System.out.print("You can get in trouble");
+			break;
+		default:
+			System.out.println("I dont know how old you are");
+			break;	
+		}
+	}
+}
