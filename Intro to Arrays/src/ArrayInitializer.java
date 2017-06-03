93d1d2eef8ebcc68214d49a15d8ce6b282491bf5
Jordan Swift <jordanswiftdev@gmail.com>
2017-06-03 16:33:00 +0100

    Add files via upload

---
+++ Intro to Arrays/src/ArrayInitializer.java
@@ -0,0 +1,13 @@
+
+public class ArrayInitializer {
+	public static void main(String[] args){
+		
+		int jordan[]={2,4,5,7,9};
+		
+		System.out.println(jordan[2]);
+		
+		CreatingArray caobj = new CreatingArray();
+		caobj.Array();
+	}
+
+}
---
+++ Intro to Arrays/src/CreatingArray.java
@@ -0,0 +1,14 @@
+
+public class CreatingArray {
+	public void Array(){
+		String list[] = new String[5];
+		list[0]="First";
+		list[1]="Second";
+		list[2]="Third";
+		list[3]="Fourth";
+		list[4]="Fifth";
+		
+		System.out.println(list[2]);
+	}
+
+}
