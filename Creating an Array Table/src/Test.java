2f9636488c07b0f86725d44ba41a77907e77cd57
Jordan Swift <jordanswiftdev@gmail.com>
2017-06-03 19:08:30 +0100

    Add files via upload

---
+++ Creating an Array Table/src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+	public static void main(String[] args){
+		System.out.println("Index\tValue");
+		int jordan[]={32,12,18,54,2};
+		
+		for(int counter=0;counter<jordan.length;counter++){
+		System.out.println(counter + "\t" + jordan[counter]);
+		}
+	}
+}
