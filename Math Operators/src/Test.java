82b9fafd92ea15a048cd8ae6c3224f1f7aa6395f
Jordan Swift <jordanswiftdev@gmail.com>
2017-06-03 11:10:08 +0100

    Add files via upload

---
+++ Math Operators/src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+	public static void main(String args[]){
+		
+		int girls, boys, people;
+		girls = 7;
+		boys = 3;
+		people = girls % boys; //% finds a remainder
+		System.out.println(people);
+	}
+
+}
