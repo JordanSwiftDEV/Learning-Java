204daa3425c6e0b2a667b8c040a49f8b79caa6b6
Jordan Swift <jordanswiftdev@gmail.com>
2017-06-06 16:23:21 +0100

    Update Test.java

--- Multidimensional Arrays/src/Test.java
+++ Multidimensional Arrays/src/Test.java
@@ -1,7 +1,7 @@
 
 public class Test {
 	public static void main(String[] args){
-		int firstarray[][]={{8,9,10,11},{12,14,13,14,15}}; //firstarray[0][0] = 8, secondarray[3][1]= 5
+		int firstarray[][]={{8,9,10,11},{12,14,13,14,15}}; //firstarray[0][0] = 8, secondarray[2][1]= 5
 		int secondarray[][]={{30,31,32,33},{43},{4,5,6}};
 	}
 
